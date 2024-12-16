a773c3e775d8fcbc65326f2140cca8ef901a4201
sadrisso <sadriisso304@gmail.com>
2024-12-16 08:52:03 +0600

    Introduction to middleware and verify token before providing data

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,7 +9,29 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
+
+const verifyToken = (req, res, next) => {
+  console.log("Inside the varify token", req.cookies)
+  const token = req?.cookies?.token
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized token!!" })
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Unauthorized token" })
+    }
+    next();
+  })
+}
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -46,8 +69,13 @@ async function run() {
     //Token releted apis
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, 'secret', {expiresIn: '1hr'})
-      res.send(token)
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5hr" })
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false
+      })
+        .send({ success: true })
     })
 
 
@@ -59,9 +87,11 @@ async function run() {
     })
 
 
-    app.get("/job-applications", async (req, res) => {
+    app.get("/job-applications", verifyToken, async (req, res) => {
       const email = req.query.email
       const filter = { applicant_email: email }
+
+      console.log("cuk cuk cookies", req.cookies)
       const result = await applicationCollection.find(filter).toArray()
 
       for (const application of result) {
