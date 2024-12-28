ff332a0872092be522cb57eaf053219290f0dd8d
sadrisso <sadriisso304@gmail.com>
2024-12-28 21:01:05 +0600

    search functionality added

--- job-portal-client/src/pages/AllJobs.jsx
+++ job-portal-client/src/pages/AllJobs.jsx
@@ -7,22 +7,38 @@ const AllJobs = () => {
 
     const [jobs, setJobs] = useState([]);
     const [sort, setSort] = useState(false);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
-        axios.get(`http://localhost:8000/all-jobs?sort=${sort}`)
+        axios.get(`http://localhost:8000/all-jobs?sort=${sort}&search=${search}`)
             .then(res => setJobs(res.data))
-    }, [sort])
+    }, [sort, search])
 
 
     return (
         <div className="mt-20">
             <div className="flex justify-between mb-10 px-2">
                 <h1 className="text-4xl font-bold text-center text-gray-600">All jobs</h1>
-                <button onClick={() => setSort(!sort)} className={ `btn btn-neutral ${sort && "btn-info"}`}>
-                    {
-                        sort ? "Sorted By Price" : "Sort by price"
-                    }
-                </button>
+                <div className="flex gap-2">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <input onKeyUp={(e) => setSearch(e.target.value)} type="text" className="grow" placeholder="Search" />
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                                fillRule="evenodd"
+                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </label>
+                    <button onClick={() => setSort(!sort)} className={`btn btn-neutral ${sort && "btn-info"}`}>
+                        {
+                            sort ? "Sorted By Price" : "Sort by price"
+                        }
+                    </button>
+                </div>
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-4 gap-5 p-2">
